5bedac1fb12969fb2523abe05b14c2a9bbb87823
byron <byron@ganguo.hk>
2019-01-29 10:48:30 +0800

    feat: 调整shopify授权的token存储及跳转设置

--- laravel/Modules/Shopify/Http/Controllers/ShopifyAuthController.php
+++ laravel/Modules/Shopify/Http/Controllers/ShopifyAuthController.php
@@ -17,11 +17,15 @@ class ShopifyAuthController extends Controller
             abort(400, '未配置shopify信息');
             \Log::info('未配置shopify信息');
         }
+        // 权限设置
         $scopes = 'write_checkouts,read_checkouts,read_products,write_orders,read_orders';
         if(isset($_GET['code'])) {
             // 获取授权token
             $accessToken = AuthHelper::getAccessToken();
-            \Log::info($config['ShopUrl']. ' access_token:' . $accessToken);
+            // 永久缓存存储授权token值
+            \Cache::forever($config['ShopUrl'].':access_token', $accessToken);
+            // 授权成功, 重定向到shopify的后台管理页面
+            return redirect(ShopifySDK::$config['AdminUrl']);
         } else {
             return redirect(AuthHelper::createAuthRequest($scopes, request()->url(), null, null, true));
         }
