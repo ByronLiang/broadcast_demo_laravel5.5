4ab12e9acc6b1a1a83c1a7e1bd765b71fb9b32a6
byron <byron@ganguo.hk>
2019-02-22 12:14:59 +0800

    feat: 增加涉及Redis事务及管道模式的相关例子

--- laravel/app/Http/Controllers/API/RedisController.php
+++ laravel/app/Http/Controllers/API/RedisController.php
@@ -42,13 +42,73 @@ class RedisController extends Controller
 	**/
     public function storeKey()
     {
-        // Redis::set('name', 'byron');
+        // $zpopped = $this->zpop('zset');
+        // \Log::info($zpopped);
+        // return \Response::success();
+        
+        // 事务乐观锁 CAS 写法
+        // 可以不设置Options
+        $options = [
+            'cas' => true,
+            'watch' => 'user:name:1',
+        ];
+        $res = Redis::transaction($options, function ($trans) {
+            $trans->set('user:name:2', 'Jay');
+            $trans->get('name');
+            $trans->multi();
+            $trans->set('user:name:1', 'superMan');
+            $trans->hmget('user:2', 'total');
+        });
+
+        return \Response::success($res);
+
+        // 非事务型的管道模式处理批量操作
+        $res = Redis::pipeline(function ($pip) {
+            $pip->set('user:1:name', 'Bill');
+            $pip->hmset('user:2', [
+                'item' => 'sport123', 
+                'total' => '33',
+            ]);
+            $pip->get('name');
+        });
+
+        // 将每个批量命令的结构以数组方式返回
+        return \Response::success($res);
+        
+        // 面向过程
+        // 事务型处理批量操作; 能确保原子性
+        // 发生错误, 所有批量操作都不能生效
+        Redis::MULTI();
+        Redis::set('name', 'byron111');
         // Redis::hmset('category', '1', 'book', '2', 'film');
-        Redis::hmset('category', [
-            '3' => 'sport', 
-            '4' => 'music',
+        Redis::hmset('user:2', [
+            'item' => 'sport', 
+            'total' => '23',
         ]);
+        Redis::EXEC();
 
         return \Response::success();
+    }
+
+    protected function zpop($key)
+    {
+        $element = null;
+        $options = array(
+            'cas' => true,      // Initialize with support for CAS operations
+            'watch' => $key,    // Key that needs to be WATCHed to detect changes
+            'retry' => 3,       // Number of retries on aborted transactions, after
+                                // which the client bails out with an exception.
+        );
+
+        Redis::transaction($options, function ($tx) use ($key, &$element) {
+            @list($element) = $tx->zrange($key, 0, 0);
+
+            if (isset($element)) {
+                $tx->multi();   // With CAS, MULTI *must* be explicitly invoked.
+                $tx->zrem($key, $element);
+            }
+        });
+
+        return $element;
     }
 }
